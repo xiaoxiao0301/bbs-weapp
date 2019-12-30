51ae8539570be0fcc3f6df983abcf6e84d7d698a
xiaoxiao <jixiaoxiao0301@163.com>
2019-12-30 18:07:14 +0800

    finish

--- wepy.config.js
+++ wepy.config.js
@@ -41,6 +41,13 @@ module.exports = {
     }
   },
   plugins: {
+    replace: {
+      filter: /\.js$/,
+      config: {
+        find: /__BASE_URL__/g,
+        replace: prod ? "'https://api.jixiaoxiao.com/api'" : "'http://bbs.com/api'"
+      }
+    }
   },
   appConfig: {
     noPromiseAPI: ['createSelectorQuery']
@@ -68,6 +75,13 @@ if (prod) {
         png: {
           quality: 80
         }
+      }
+    },
+    replace: {
+      filter: /\.js$/,
+      config: {
+        find: /__BASE_URL__/g,
+        replace: prod ? "'https://api.jixiaoxiao.com/api'" : "'http://bbs.com/api'"
       }
     }
   }
